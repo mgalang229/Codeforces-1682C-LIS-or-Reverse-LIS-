1fda86dd387a802d48e49a7d8dfc1b35cff1b834
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-01-01 16:52:28 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -3,20 +3,6 @@ using namespace std;
 
 /*
 
-2 5 4 5 2 4
-2 4 5 5 4 2
-
-palindrome of increasing numbers
-first half -> 1 to k
-second half -> n to k
-
-2 2 2 4 4 5 5 5
-2 4 5 5 5 4 2 2
-
-2 = 3
-4 = 2
-5 = 3
-
 1 3 3 2
 solo = 2
 not solo = 1
